a52bff21600a90cb33c043953281efdc48bc0ef8
hukut03 <nseryakov13@gmail.com>
2023-01-24 23:18:46 +0300

    first commit for home work2

---
+++ HomeWork2.java
@@ -0,0 +1,18 @@
+package OtusHomeWork2;
+
+import java.util.Base64;
+import java.util.Scanner;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your full name");
+        String fullName = scanner.nextLine();
+        //String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
+        /*
+        Не совсем понял что делают эти методы
+         */
+        System.out.println("Program result: " + fullName);
+    }
+
+}
