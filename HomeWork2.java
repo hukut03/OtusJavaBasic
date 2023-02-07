00ee33414ff05891aaec7191d8d711e86e79d51b
hukut03 <nseryakov13@gmail.com>
2023-02-07 23:31:21 +0300

    Otus HomeWork3

--- HomeWork2.java
+++ HomeWork2.java
@@ -1,4 +1,4 @@
-package OtusHomeWork2;
+package OtusHomeWork;
 
 import java.util.Base64;
 import java.util.Scanner;
@@ -8,11 +8,11 @@ public class HomeWork2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your full name");
         String fullName = scanner.nextLine();
-        //String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
+        String encodedFullName = Base64.getEncoder().encodeToString(fullName.getBytes());
         /*
         Не совсем понял что делают эти методы
          */
-        System.out.println("Program result: " + fullName);
+        System.out.println("Program result: " + encodedFullName );
     }
 
 }
--- OtusHomeWork3.java
+++ OtusHomeWork3.java
@@ -14,34 +14,32 @@ public class OtusHomeWork3 {
                 {"1 answer", "!!! 2 answer!!!", "3 answer"},
                 {"1 answer", "2 answer", "!!! 3 answer!!!", "4 answer", "5 answer"}
         };
+
         int[] correctAnswers = {1, 2, 3, 4, 5};
 
-        System.out.println(questions[0]);
-        System.out.println(Arrays.toString(answerOptions[0]));
-        Scanner scanner = new Scanner(System.in);
-        int str = scanner.nextInt();
-        if (str==(correctAnswers[0])) {
-            correctCount++;
-        } else wrongCount++;
-
-
-        System.out.println(questions[1]);
-        System.out.println(Arrays.toString(answerOptions[1]));
-        Scanner scanner1 = new Scanner(System.in);
-        int str1 = scanner.nextInt();
-        if (str1==(correctAnswers[1])) {
-            correctCount++;
-        } else wrongCount++;
-
-        System.out.println(questions[2]);
-        System.out.println(Arrays.toString(answerOptions[2]));
-        Scanner scanner2 = new Scanner(System.in);
-        int str2 = scanner.nextInt();
-        if (str2==(correctAnswers[2])) {
-            correctCount++;
-        } else wrongCount++;
-
-        System.out.println();
-        System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            for (int j = 0; j < answerOptions[j].length; j++) {
+                System.out.println(Arrays.toString(answerOptions[i]));
+                break;
+            }
+            System.out.println("Твой ответ: ");
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNextInt()) {
+                int str = scanner.nextInt();
+                if (str == correctAnswers[i]) {
+                    correctCount++;
+                } else
+                    wrongCount++;
+            } else {
+                System.out.println("Формат ответа не коректный, введи ответ заново: ");
+                int str = scanner.nextInt();
+                if (str == correctAnswers[i]) {
+                    correctCount++;
+                } else
+                    wrongCount++;
+            }
+            System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
+        }
     }
 }
